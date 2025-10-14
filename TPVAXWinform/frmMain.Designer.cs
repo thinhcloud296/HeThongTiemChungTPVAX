8a0951dcb12c51dceddfdfd2c06600d7d6c6dc60
thinhcloud296 <nguyenhoangthinh2906@gmail.com>
2025-10-14 21:08:04 +0700

    Create Winform and update HomePage

---
+++ TPVAXWinform/frmMain.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace TPVAXWinform
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.logo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbHoTenNVDangNhap = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbMaNVDangNhap = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbChucVuNVDangNhap = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            this.splitContainer1.Panel1.Controls.Add(this.logo);
+            this.splitContainer1.Panel1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.splitContainer1.Size = new System.Drawing.Size(1746, 838);
+            this.splitContainer1.SplitterDistance = 314;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // logo
+            // 
+            this.logo.BackgroundImage = global::TPVAXWinform.Properties.Resources.logo;
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logo.Location = new System.Drawing.Point(3, 3);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(308, 100);
+            this.logo.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 248);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 587);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lbChucVuNVDangNhap);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.lbMaNVDangNhap);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lbHoTenNVDangNhap);
+            this.panel2.Location = new System.Drawing.Point(6, 109);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(305, 133);
+            this.panel2.TabIndex = 2;
+            // 
+            // lbHoTenNVDangNhap
+            // 
+            this.lbHoTenNVDangNhap.AutoSize = true;
+            this.lbHoTenNVDangNhap.Location = new System.Drawing.Point(113, 66);
+            this.lbHoTenNVDangNhap.Name = "lbHoTenNVDangNhap";
+            this.lbHoTenNVDangNhap.Size = new System.Drawing.Size(51, 20);
+            this.lbHoTenNVDangNhap.TabIndex = 0;
+            this.lbHoTenNVDangNhap.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Họ tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã nhân viên";
+            // 
+            // lbMaNVDangNhap
+            // 
+            this.lbMaNVDangNhap.AutoSize = true;
+            this.lbMaNVDangNhap.Location = new System.Drawing.Point(113, 38);
+            this.lbMaNVDangNhap.Name = "lbMaNVDangNhap";
+            this.lbMaNVDangNhap.Size = new System.Drawing.Size(158, 20);
+            this.lbMaNVDangNhap.TabIndex = 3;
+            this.lbMaNVDangNhap.Text = "Nguyễn Hoàng Thịnh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Chức vụ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbChucVuNVDangNhap
+            // 
+            this.lbChucVuNVDangNhap.AutoSize = true;
+            this.lbChucVuNVDangNhap.Location = new System.Drawing.Point(113, 94);
+            this.lbChucVuNVDangNhap.Name = "lbChucVuNVDangNhap";
+            this.lbChucVuNVDangNhap.Size = new System.Drawing.Size(51, 20);
+            this.lbChucVuNVDangNhap.TabIndex = 5;
+            this.lbChucVuNVDangNhap.Text = "label4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Thông tin nhân viên";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(302, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "HỒ SƠ TIÊM CHỦNG";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(177, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Các Danh Mục Quản Lý";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(302, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "KHÁCH HÀNG";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(302, 61);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "VACCINE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(302, 61);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "NHÀ CUNG CẤP";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 318);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(302, 61);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "NHÂN VIÊN";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 385);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(302, 61);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "HÓA ĐƠN";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 452);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(302, 61);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "KHUYẾN MÃI";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(3, 517);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(302, 61);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "THỐNG KÊ";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1746, 838);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.main_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel logo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbChucVuNVDangNhap;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbMaNVDangNhap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbHoTenNVDangNhap;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
