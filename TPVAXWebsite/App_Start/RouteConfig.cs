872ae9df611af68a6f1a3fdaae22f494c7e78b52
Tran Tan Tai <taitran.february@gmail.com>
2025-11-20 06:23:47 +0700

    chuyển full html sang cshtml, bắt đầu backend từ con số 0

--- TPVAXWebsite/App_Start/RouteConfig.cs
+++ TPVAXWebsite/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace TPVAXWebsite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+                //defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
             );
         }
     }
