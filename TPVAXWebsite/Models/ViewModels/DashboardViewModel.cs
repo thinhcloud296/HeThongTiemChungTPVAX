5be78ce507ed46ba70afee5681ff9e242623f106
VP4304 <148425056+VP4304@users.noreply.github.com>
2025-11-22 14:15:39 +0700

    Add files via upload

---
+++ TPVAXWebsite/Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TPVAXWebsite.Models.Domain;
+
+namespace TPVAXWebsite.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public KhachHang KhachHang { get; set; }
+        public List<HoSoTiemChung> HoSoTiemChungs { get; set; }
+        public int SoMuiHoanThanh { get; set; }
+        public List<LichTiem> LichHenSapToi { get; set; }
+        public List<LichTiem> LichTiems { get; set; }
+        // ✅ Đã hủy — thêm dòng này
+        public List<LichTiem> LichDaHuy { get; set; } = new List<LichTiem>();
+
+
+    }
+
+
+}
