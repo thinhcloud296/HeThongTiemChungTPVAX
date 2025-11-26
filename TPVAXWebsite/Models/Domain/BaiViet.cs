0b1b856e28b00fc196ec507c2f1c8b2177e10afd
VP4304 <148425056+VP4304@users.noreply.github.com>
2025-11-27 00:36:47 +0700

    Trang Kiến Thức Tiêm Chủng

---
+++ TPVAXWebsite/Models/Domain/BaiViet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TPVAXWebsite.Models.Domain
+{
+    [Table("BaiViet")]
+    public class BaiViet
+    {
+        [Key]
+        public int MaBV { get; set; }
+        public string TieuDe { get; set; }
+        public string TomTat { get; set; }
+        public string NoiDung { get; set; }
+        public string HinhAnh { get; set; }
+        public string DanhMuc { get; set; }
+        public string Tag { get; set; }
+        public DateTime NgayDang { get; set; }
+        public bool TrangThai { get; set; }
+    }
+}
