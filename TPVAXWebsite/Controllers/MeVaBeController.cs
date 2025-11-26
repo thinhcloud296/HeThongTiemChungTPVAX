bf816ebca5e2294516e47e76fb7aee60f2653e9a
VP4304 <148425056+VP4304@users.noreply.github.com>
2025-11-27 00:40:51 +0700

    Trang Kiến Thức Tiêm Chủng

--- TPVAXWebsite/Controllers/MeVaBeController.cs
+++ TPVAXWebsite/Controllers/MeVaBeController.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using System.Web.Mvc;
+using TPVAXWebsite.Services;
 
 namespace TPVAXWebsite.Controllers
 {
@@ -7,11 +9,31 @@ namespace TPVAXWebsite.Controllers
     /// </summary>
     public class MeVaBeController : Controller
     {
-        // GET: MeVaBe/Index
+        private readonly BaiVietService _service = new BaiVietService();
+
         public ActionResult Index()
         {
-            // TODO: Load kiến thức về mẹ và bé
-            return View();
+            var ds = _service.LayTheoDanhMuc("Mẹ và bé");
+            return View(ds);
         }
+
+        public ActionResult ChiTiet(int id)
+        {
+            var bv = _service.LayChiTiet(id);
+            if (bv == null) return HttpNotFound();
+
+            // Lấy 5 bài viết bất kỳ trong chuyên mục "Mẹ và bé"
+            var lienQuan = _service.LayTheoDanhMuc("Mẹ và bé")
+                                   .Where(x => x.Id != id)   // loại trừ bài hiện tại
+                                   .OrderByDescending(x => x.NgayDang)
+                                   .Take(5)
+                                   .ToList();
+
+            ViewBag.LienQuan = lienQuan;
+
+            return View(bv);
+        }
+
     }
+
 }
