74703c17e245e08616ff8767d9732b1bed15e24c
VP4304 <148425056+VP4304@users.noreply.github.com>
2025-11-27 20:40:45 +0700

    lỗi do thiếu ViewModels

---
+++ TPVAXWebsite/Models/ViewModels/VaccineRecommendationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPVAXWebsite.Models.ViewModels
+{
+    public class VaccineRecommendationViewModel
+    {
+        public string MaVC { get; set; }
+        public string TenVC { get; set; }
+        public string MoTa { get; set; }
+        public decimal GiaBan { get; set; }
+        public string HinhAnh { get; set; }
+    }
+}
